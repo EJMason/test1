2885917e96426e1df10d0233fdb5b249d59ef66a
EJ Mason <ej-mason@pluralsight.com>
2020-07-02 15:59:41 -0600

    fix: 🐛 Make config publish to github registry

--- release.config.js
+++ release.config.js
@@ -7,8 +7,8 @@ module.exports = {
         '@semantic-release/release-notes-generator',
         // https://github.com/semantic-release/changelog
         ['@semantic-release/changelog', { changelogFile: 'docs/CHANGELOG.md' }],
-        // https: //github.com/semantic-release/npm
-        ['@semantic-release/npm', { npmPublish: false }],
+        // https://github.com/semantic-release/npm
+        ['@semantic-release/npm', { npmPublish: true }],
         // https://github.com/semantic-release/git
         [
             '@semantic-release/git',
